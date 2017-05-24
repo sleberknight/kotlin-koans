e49cafe8d8de88129ac7f64f2b794c2a3c9fe9b2
Scott Leberknight <scottleber@gmail.com>
2017-05-23 21:53:28 -0400

    Complete koan 29 (Operator Overloading)

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -12,11 +12,20 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+operator fun MyDate.plus(interval: TimeInterval): MyDate = addTimeIntervals(interval, 1)
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+
+class RepeatedTimeInterval(val interval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) =
+        addTimeIntervals(repeatedTimeInterval.interval, repeatedTimeInterval.number)
 
 class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> = MyDateIterator(this)
