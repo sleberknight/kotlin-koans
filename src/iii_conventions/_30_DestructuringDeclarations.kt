0972a8ea4fc8308755150a8ececd7adeb5c4f42b
Scott Leberknight <scottleber@gmail.com>
2017-05-23 22:00:53 -0400

    Complete koan 30 (Destructuring Declarations)

--- src/iii_conventions/_30_DestructuringDeclarations.kt
+++ src/iii_conventions/_30_DestructuringDeclarations.kt
@@ -13,10 +13,13 @@ fun todoTask30(): Nothing = TODO(
 
 class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
+operator fun MyDate.component1() = year
+operator fun MyDate.component2() = month
+operator fun MyDate.component3() = dayOfMonth
+
 fun isLeapDay(date: MyDate): Boolean {
-    todoTask30()
-//    val (year, month, dayOfMonth) = date
-//
-//    // 29 February of a leap year
-//    return year % 4 == 0 && month == 2 && dayOfMonth == 29
+    val (year, month, dayOfMonth) = date
+
+    // 29 February of a leap year
+    return year % 4 == 0 && month == 2 && dayOfMonth == 29
 }
